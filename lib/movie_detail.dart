d3fa6f1316196287fa199b7d15128502c7a0958b
kkwabaegi <0412seg@naver.com>
2023-06-09 09:01:37 +0900

    asd

--- lib/movie_detail.dart
+++ lib/movie_detail.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_kobis/movie_api.dart';
 
-class MovieDetail extends StatefulWidget {
+class MovieDetail extends StatelessWidget {
   String movieCd;
 
   MovieDetail({super.key, required this.movieCd});
 
   @override
-  State<MovieDetail> createState() => _MovieDetailState();
-}
+  build(BuildContext context) {
+    // var movieApi = MovieApi();
+    // var movie = await movieApi.searchDetail(moviecode: movieCd);
 
-class _MovieDetailState extends State<MovieDetail> {
-  @override
-  build(BuildContext context) async {
-    var movieApi = MovieApi();
-    var movie = await movieApi.searchDetail(moviecode: widget.movieCd);
-
-    var movieTitle = movie['movieNm'];
+    // var movieTitle = movie['movieNm'];
+    // print(movieTitle);
     /* dynamic titleImage = movie['posters'].toString().isEmpty
         ? Image.asset('assets/images/no_image.png')
         : Image.network(movie['posters'].toString().split('|')[0]); */
@@ -25,35 +20,35 @@ class _MovieDetailState extends State<MovieDetail> {
     //만약 없으면 로컬 이미지
     //있으면 네트워크 이미지
     return Scaffold(
-      appBar: AppBar(title: Text('영화 상세 정보($movieTitle)')),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 100,
-                child: Center(
-                  child: Text(movieTitle,
-                      style: const TextStyle(
-                          fontSize: 50, fontWeight: FontWeight.bold)),
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                      child: Text(
-                    '키워드 : ${movie['typeNm']}',
-                    style: const TextStyle(
-                      fontSize: 20,
-                    ),
-                  )),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+      appBar: AppBar(title: const Text('영화 상세 정보')),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(10),
+      //   child: SingleChildScrollView(
+      //     child: Column(
+      //       children: [
+      //         SizedBox(
+      //           height: 100,
+      //           child: Center(
+      //             child: Text(movieTitle,
+      //                 style: const TextStyle(
+      //                     fontSize: 50, fontWeight: FontWeight.bold)),
+      //           ),
+      //         ),
+      //         Row(
+      //           children: [
+      //             Expanded(
+      //                 child: Text(
+      //               '키워드 : ${movie['typeNm']}',
+      //               style: const TextStyle(
+      //                 fontSize: 20,
+      //               ),
+      //             )),
+      //           ],
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
